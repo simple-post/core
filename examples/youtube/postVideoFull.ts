c4f9ef40a413b43536ae0a1ddb35339e379458fd
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2025-06-23 00:23:34 +0200

    feat: youtube examples

---
+++ examples/youtube/postVideoFull.ts
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import { post } from "unsubpost";
+
+// Load environment variables from .env file
+dotenv.config();
+
+async function main() {
+  const results = await post({
+    content: {
+      media: [
+        {
+          type: "video",
+          path: "./youtube/data/horizontal.mp4",
+          title: "Test video full",
+          description: "This is a test video with all parameters",
+          thumbnailPath: "./youtube/data/horizontal.jpg",
+        },
+      ],
+      options: {
+        privacyStatus: "unlisted",
+        youtubeSpecific: {
+          tags: ["test", "video"],
+          categoryId: "10",
+          selfDeclaredMadeForKids: true,
+        },
+      },
+    },
+    platforms: ["youtube"],
+  });
+
+  console.log(results);
+}
+
+main();

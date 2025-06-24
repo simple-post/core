5aca7680ee673382633d6af8d65bf1d04a69cfd3
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2025-06-25 01:15:29 +0200

    feat: add tests for Telegram

--- lib/tests/setup.ts
+++ lib/tests/setup.ts
@@ -6,3 +6,4 @@ process.env.TWITTER_API_KEY = "test_api_key";
 process.env.TWITTER_API_SECRET = "test_api_secret";
 process.env.TWITTER_ACCESS_TOKEN = "test_access_token";
 process.env.TWITTER_ACCESS_SECRET = "test_access_secret";
+process.env.TELEGRAM_BOT_TOKEN = "test_telegram_bot_token";

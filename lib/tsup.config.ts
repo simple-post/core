0286eb65acf459b27c1daf410b7daea939129419
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2025-07-21 15:20:42 +0200

    fix: unsubpost package usable from JS

---
+++ lib/tsup.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  minify: false,
+  target: "node16",
+  outDir: "dist",
+  treeshake: true,
+  external: [
+    "@aws-sdk/client-s3",
+    "@aws-sdk/lib-storage",
+    "axios",
+    "form-data",
+    "googleapis",
+    "twitter-api-v2",
+    "uuid",
+    "zod",
+  ],
+});
